1b95564cf91c20d000ef946e026a47a22f7267a9
Bartosz <par-gluten-0f@icloud.com>
2026-03-17 18:31:27 +0100

    Initial Commit

---
+++ BackendTesting/BackendTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackendTestingApp.swift
+//  BackendTesting
+//
+//  Created by Bartosz Mrugała on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct BackendTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackendTesting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BackendTesting
+//
+//  Created by Bartosz Mrugała on 17/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
